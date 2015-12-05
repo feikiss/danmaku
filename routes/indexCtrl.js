e6aa7c324528564b3015759e36a93dc0c6ac946c
xiaofli <xiaofli@tibco-support.com>
2015-12-05 16:05:39 +0800

    test to edit.

--- routes/indexCtrl.js
+++ routes/indexCtrl.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{title:"danmaku"});
+  res.render('index',{title:"danmaku1234"});
 });
 
 module.exports = router;
